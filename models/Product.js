d0fd320d1ee9df9796d540d97c7c842923c8f020
Heather Crawford <heather.crawford.ela@gmail.com>
2023-12-19 14:36:21 -0600

    set up product columns

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,  
+      autoIncrement: true    
+    },
+    product_name: {
+      type: Datatypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(12,2),
+      allowNull: false,
+      validate: {isDecimal: true,}
+    },
+    stock: {
+      type: Data.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {isNumeric: true}
+    },
+    category_id: {
+      type:DataTypes.INTEGER,
+      references: {
+        model:"category",
+        key: "id"
+      }
+    }
+
   },
   {
     sequelize,
